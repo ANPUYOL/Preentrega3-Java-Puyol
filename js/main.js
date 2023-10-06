4de8481f2946134fd91b76408e20c46e51d0fed2
Analia Puyol <anaveronica8204@gmail.com>
2023-10-06 00:46:29 -0300

    json

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 let productos = [];
 
+fetch("./js/productos.json")
+    .then(response => response.json())
+    .then(data => {
+        productos = data;
+        cargarProductos(productos);
+    })
+
+
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
 const tituloPrincipal = document.querySelector("#titulo-principal");
